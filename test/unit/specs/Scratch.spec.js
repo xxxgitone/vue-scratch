47a23786b769ed94ba3b2eb3a33a46edb85cd302
xxxgitone <xxxgit@sina.com>
2018-04-13 17:47:01 +0800

    test: propsData

--- test/unit/specs/Scratch.spec.js
+++ test/unit/specs/Scratch.spec.js
@@ -2,7 +2,19 @@ import Vue from 'vue'
 import VueScratch from '../../../src/component/vue-scratch.vue'
 
 describe('Vue-Scratch', () => {
-  it ('data should be a function', () => {
-    expect(typeof VueScratch.data).to.equal('function')
+
+  it('renders the correct width and height', () => {
+    const Constructor = Vue.extend(VueScratch)
+    const vm = new Constructor({
+      propsData: {
+        coverWidth: 300,
+        coverHeight: 200
+      }
+    }).$mount()
+
+    expect(vm.$el.style.width).to.be.equal('300px')
+    expect(vm.$el.style.height).to.be.equal('200px')
+    expect(vm.$refs.canvas.getAttribute('width')).to.be.equal('300')
+    expect(vm.$refs.canvas.getAttribute('height')).to.be.equal('200')
   })
 })
